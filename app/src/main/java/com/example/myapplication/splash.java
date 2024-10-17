b95ca2fab8d0ef4260399c9303fcb2bd16070123
cao-Dat04 <2251120204@ut.edu.vn>
2024-10-18 01:36:25 +0700

    Tạo màng hình chào mừng(Splash Screen)

--- app/src/main/java/com/example/myapplication/splash.java
+++ app/src/main/java/com/example/myapplication/splash.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,15 @@ public class splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
 }
